55905dee18a5cb8f40665a0ec75559beb1569558
Santosh Telkar <santelhello@gmail.com>
2025-03-21 11:34:59 -0700

    Multi package path based trigger for single workflow

---
+++ package1/test_package1.py
@@ -0,0 +1,4 @@
+from package1.package1 import add
+
+def test_add():
+    assert add(2, 3) == 5
---
+++ package2/test_package2.py
@@ -0,0 +1,4 @@
+from package2.package2 import multiply
+
+def test_multiply():
+    assert multiply(2, 3) == 6
